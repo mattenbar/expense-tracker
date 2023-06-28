a021cca883256ac738b17c620340a7db3e7aa7dc
matt enbar <matthew.enbar@gmail.com>
2023-06-28 17:57:13 -0400

    S5 L78

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -16,8 +16,19 @@ export default function ExpenseForm() {
     }
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    }
+    console.log(expenseData)
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
